05b5e6811e00c7c1a14922dd1bf068b2f272bd92
sarahmr63089 <sarahrose63089@gmail.com>
2020-08-11 20:14:26 -0400

    Done.

--- README.md
+++ README.md
@@ -13,8 +13,7 @@ npm install -g json-server
 Then run the server with:
 
 ```bash
-json-server db.json
-```
+ ```
 
 This will serve your code on `http://localhost:3000`.
 
@@ -24,10 +23,10 @@ This will serve your code on `http://localhost:3000`.
 
 You will be using the following backend to get the list of books. The expectation here is that you will include the following features
 
-- Get a list of books & render them
-  `http://localhost:3000/books`
-- Be able to click on a book, you should see the book's thumbnail and description and a list of users who have liked the book.
-- You can like a book by clicking on a button. You are user 1 `{"id":1, "username":"pouros"}`, so to like a book send a `PATCH` request to `http://localhost:3000/books/:id` with an array of users who like the book. This array should be equal to the existing array of users that like the book, plus your user. For example, if the previous array was `"[{"id":2, "username":"auer"}, {"id":8, "username":"maverick"}]`, you should send as the body of your PATCH request:
+<!-- - Get a list of books & render them
+  `http://localhost:3000/books` -->
+<!-- - Be able to click on a book, you should see the book's thumbnail and description and a list of users who have liked the book. -->
+<!-- - You can like a book by clicking on a button. You are user 1 `{"id":1, "username":"pouros"}`, so to like a book send a `PATCH` request to `http://localhost:3000/books/:id` with an array of users who like the book. This array should be equal to the existing array of users that like the book, plus your user. For example, if the previous array was `"[{"id":2, "username":"auer"}, {"id":8, "username":"maverick"}]`, you should send as the body of your PATCH request:
 
 ```javascript
 {
@@ -39,7 +38,7 @@ You will be using the following backend to get the list of books. The expectatio
 }
 ```
 
-- This route will respond with the updated book json including the list of users who have liked the book.
-- BONUS: Can you make it so a second patch request to the same book removes your user from the list of users? Can you toggle likes on and off?
+- This route will respond with the updated book json including the list of users who have liked the book. -->
+<!-- - BONUS: Can you make it so a second patch request to the same book removes your user from the list of users? Can you toggle likes on and off? -->
 
 [json-server]: https://github.com/typicode/json-server
--- js/index.js
+++ js/index.js
@@ -1,1 +1,113 @@
 document.addEventListener("DOMContentLoaded", function() {});
+
+let bookList = document.querySelector("ul#list")
+let bookShow = document.querySelector("div#show-panel")
+let bookArr = []
+
+fetch("http://localhost:3000/books")
+.then(res => res.json())
+.then((bookObjArr) => {
+  bookArr = bookObjArr
+  renderBookList()
+})
+
+let bookListHTML = (bookObj) => {
+  let bookLi = document.createElement("li")
+  bookLi.innerText = bookObj.title
+
+  bookList.append(bookLi)
+
+  bookLi.addEventListener("click", (evt) => {
+    bookHTML(bookObj)
+  })
+}
+
+let renderBookList = () => {
+  bookArr.forEach((book) => {
+    bookListHTML(book)
+  })
+}
+
+
+let bookHTML = (bookObj) => {
+  bookShow.innerHTML = ""
+  let bookImg = document.createElement("img")
+  bookImg.src = bookObj.img_url
+
+  let bookTitle = document.createElement("h2")
+  bookTitle.innerText = bookObj.title
+
+  let bookSubTitle = document.createElement("h2")
+  bookSubTitle.innerText = bookObj.subtitle
+
+  let bookAuthor = document.createElement("h2")
+  bookAuthor.innerText = bookObj.author
+
+  let bookDes = document.createElement("p")
+  bookDes.innerText = bookObj.description
+
+  let userList = document.createElement("ul")
+
+  bookObj.users.forEach((user) => {
+    let userLi = document.createElement("li")
+    userLi.innerText = user.username
+
+    userList.append(userLi)
+  })
+
+  let likeButton = document.createElement("button")
+  if ( bookObj.users.find(user => user.id === 1) ) {
+    // user with id 1
+    likeButton.innerText = "Unlike"
+  }
+  else {
+    likeButton.innerText = "Like"
+  }
+
+  bookShow.append(bookImg, bookTitle, bookSubTitle, bookAuthor, bookDes, userList, likeButton)
+
+
+  likeButton.addEventListener("click", (evt) => {
+    // update in memory, database, on page
+    if (likeButton.innerText === "Like") {
+      likeButton.innerText = "Unlike"
+
+      bookObj.users.push({"id":1, "username":"pouros"})
+
+      fetch(`http://localhost:3000/books/${bookObj.id}`, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({
+          users: bookObj.users
+        })
+      })
+      .then(res => res.json())
+      .then((updatedBookObj) => {
+        bookHTML(updatedBookObj)
+      })
+    }
+    else {
+      likeButton.innerText = "Like"
+
+      bookObj.users = bookObj.users.filter((user) => { return user.id !== 1 })
+
+      fetch(`http://localhost:3000/books/${bookObj.id}`, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({
+          users: bookObj.users
+        })
+      })
+      .then(res => res.json())
+      .then((updatedBookObj) => {
+        bookHTML(updatedBookObj)
+      })
+
+    }
+  })
+}
+
